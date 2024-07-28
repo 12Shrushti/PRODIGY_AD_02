92eea14670723239b024aba3156c5e2cacc4ab05
Srushti Shete <153730931+12Shrushti@users.noreply.github.com>
2024-07-29 00:34:37 +0530

    Add files via upload

---
+++ color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static Color primaryColor = Color(0xFF00061a);
+  static Color secondaryColor = Color(0xFF97D5C9);
+  static Color accentColor = Color(0xFF4169e8);
+  static Color color = Color(0xFFf7f6f4);
+  static Color color1 =Color(0xFFfcab88);
+  static Color color2=Color(0xFFffd379);
+  // static Color color3=:
+}
